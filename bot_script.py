689ecad7c666b42adabae725ee644a134db0b8a1
alaprais <notasprinter@gmail.com>
2023-08-06 19:02:08 -0700

    finished main menu functions, starting game functions

--- bot_script.py
+++ bot_script.py
@@ -11,6 +11,9 @@ import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
+#TODO change all hardcoded pixel shit to relative coding? Or some sort of img recognition baseline file to run beforehand. 
+# That can then save all the file locations.
+
 def click(coord, double=False, time_after=.3) -> None:
     '''clicks at specified location. double clicks if double=True'''
 
@@ -24,7 +27,6 @@ def click(coord, double=False, time_after=.3) -> None:
 
     time.sleep(time_after)
 
-# Main Menu Functions
 def read_quest() -> str:
     '''reads first quest and returns objective'''
 
@@ -33,7 +35,6 @@ def read_quest() -> str:
     # TODO reroll functionality (separate func)
     # if one is less than 750 gold, reroll to 500
     # For now re-roll "kill opponents creatures"
-
     #quest 1 Point(x=805, y=871)
     #quest 2 Point(x=510, y=866)
     #quest 3 Point(x=235, y=852)
@@ -50,7 +51,7 @@ def read_quest() -> str:
 
     return objective
 
-def choose_deck(objective):
+def choose_deck(objective) -> None:
     '''chooses deck based on objective. Must be on a screen with the deckboxes'''  # TODO could this use a img classifier
 
     template_dict = {'white': 'temp', 
@@ -59,67 +60,40 @@ def choose_deck(objective):
                      'red': 'temp',
                      'green': r"C:\Users\Arnaud\projects\resources\card-game-bot-resources\objectives\green.png",
                      }
-    
     template = cv2.imread(template_dict[objective]) # img we want to match in current screen
     sct = PIL.ImageGrab.grab() # screenshot 
     sct = np.array(sct) # transformed to numpy array for cv2
     sct = cv2.cvtColor(sct, cv2.COLOR_RGB2BGR) # convert to cv2 color space, RGB to BGR
-
     res = cv2.matchTemplate(sct, template, cv2.TM_CCOEFF_NORMED) # match scores
     threshold = 0.8
-    loc = np.where(res >= threshold)
-    print(loc)
+    loc = np.where(res >= threshold) 
 
-    print(sct.shape)
-    print(res.shape)
+    left_corner = min(zip(*loc[::-1])) # pinning the left-top corner of the template here gives best match? I think thats the interp.
+    # min not working as intended, basically a random choice as far as logic is concerned but its late and it doesnt seem to matter
+    x_correct = template.shape[1] # need to move the cursor from the left edge to the center 
+    #pyautogui.moveTo(left_corner + (x_correct,0))# TODO: why does this work and not the half?
+    click(left_corner + (x_correct,0))
 
-    # click on spot
-    #pyautogui.moveTo(coord)
-
-    #####
-    # img = cv2.imread(r"C:\Users\Arnaud\projects\resources\card-game-bot-resources\img_to_match.png")
-    # template = cv2.imread(template_dict[objective])
-    # res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED) # matching
-
-    # # Specify a threshold
-    # threshold = 0.8
-    # # Store the coordinates of matched area in a numpy array
-    # loc = np.where(res >= threshold)
-    # print(loc)
-    # h, w = template.shape[0:2]
-    # # draw rectangle around matched area
-    # for pt in zip(*loc[::-1]):
-    #     cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 255, 255),2)
-
-    # cv2.imshow("Image",img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    # cv2.imshow("Template",template)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
 
 def queue(format:str='bot_match') -> None:
-    '''from main menu do a series of clicks to queue up for match in specified format ''' 
+    '''from main menu do a series of clicks to queue up for match in specified format.
+        everything up until (not including) deck selection.''' 
 
     format_to_coord = {'bot_match': (1666, 696), 
-                       'explorer': (1676, 628)}
+                       'explorer': (1676, 628)}   
     sequence = [(1739, 996), # orange play   
                 (1733, 142), # find match
                 (1743, 285), # play (unranked)
                 format_to_coord[format]] 
-    
     for coord in sequence:
         click(coord)
-    # choose deck
-    # click play
+
+def start_game() -> None:
+    click(1735, 1005) # orange play button   
 
 def is_quest_complete() -> bool:
     '''checks if quest is complete'''
-    # top left Point(x=773, y=774)
-    # top right Point(x=834, y=775)
-    # bottom left Point(x=777, y=797)
-    # bottom right Point(x=833, y=801)
+
     quest1_text_bbox = [770,775,840,800]
     img = PIL.ImageGrab.grab(quest1_text_bbox) # screenshot of quest text
     #img.show()
@@ -131,29 +105,77 @@ def is_quest_complete() -> bool:
 def restart_app() -> None:
     close("MTGA")
     open("MTG Arena")
+    time.sleep(60) # wait for app to open
 
 
-    
+# TODO: change static wait times to image recognition. ie: waiting for main menu can be characterized by certain things.
+
+###################### Bot Game Behavior
+def keep_hand() -> bool:
+    '''keep starting hand'''
+    pass
+
+def is_my_turn() -> bool:
+    '''true if my turn, false if opponents turn'''
+    # read bottom right for string "Opponents"
+    turn_text_bbox = [1681,933,1881,964]  #TODO hardcoded
+    img = PIL.ImageGrab.grab(turn_text_bbox) # screenshot 
+    #img.show()
+    img = img.convert('L') # convert to grayscale to help text recog
+    #img.show()
+    turn_text = pytesseract.image_to_string(img)
+    print(turn_text)
+    return not ("Opponent's" in turn_text)
+
+def is_interact() -> bool:
+    '''checks if opponent played interaction'''
+    pass
+
+def play_land() -> None:
+    '''play a land if possible'''
+    pass
+
+def play_spell() -> None:
+    '''play a spell if possible'''
+
+def resolve() -> None:
+    '''resolves any interaction from opponent or random game actions'''
+    pyautogui.press('space')
+
+def combat_phase() -> None:
+    '''the bots combat phase'''
+    pyautogui.press('space') #all attack
+
+def end_phase() -> None:
+    '''end current phase and move to next phase'''
+    pyautogui.press('space')
+
+######################################### Script    
 def main() -> None:
-    # Launch Game
-    # open("MTG Arena")
-    # time.sleep(60) # wait for app to open
-    time.sleep(2)
-    #choose_deck('green')
-    click((471, 423), double=True)
-    # while (len(objective) > 0): # while there are stlll quests to complete
 
-    #     choose_deck(objective='green')
+    open("MTG Arena")
+    time.sleep(30) # wait for app to open
+
+    objective = read_quest()
+    while (len(objective) > 0): # while there are still quests to complete
+
+        queue()
+        choose_deck(objective)
 
     #     # TODO: GAME
     #     # in_game = False
     #     # while(in_game): # play a game
     #     #     pass
 
-    #     objective = read_quest()
+        if is_quest_complete() == False: 
+            restart_app() # reset the UI so a fresh quest is in the first position
+        objective = read_quest()
 
     #close("MTGA")
 
 
+def main_test():
+    time.sleep(2)
+    print(is_my_turn())
 
-main()
+main_test()
